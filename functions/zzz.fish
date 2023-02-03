321c997e5ee39886c7e9d7495b86de37c0bd4ec5
Ivan Yastrebov <easy.quest@ya.ru>
2023-02-03 14:49:49 +0300

    Last Sync: 03.02.2023 14:49:49

--- functions/zzz.fish
+++ functions/zzz.fish
@@ -1,4 +1,12 @@
 function zzz -d "My package"
-  # Точка входа пакета
-  echo ttttt
+    # Точка входа пакета
+    set i (contains -i -- -d $argv; or contains -i -- --dry-run $argv);echo $i
+
+    if test -n "$i"
+        set -e argv[$i]
+
+        set dry_run 1
+        set _get_flags $_get_flags -s
+    end
+    set -e i
 end
